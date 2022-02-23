3606b3629ed005d5606ac5cc86cafb2e79fe0a89
Roberto Saback <robertosaback@Robertos-MacBook-Pro.local>
2022-02-23 04:23:44 -0300

    hello world exercice - Done

--- 01_helloWorld/helloWorld.js
+++ 01_helloWorld/helloWorld.js
@@ -1,5 +1,10 @@
+// const helloWorld = function() {
+//   return 'Hello, World!'
+// };
+
+// module.exports = helloWorld;
 const helloWorld = function() {
-  return ''
+  return 'Hello World!'
 };
 
 module.exports = helloWorld;
